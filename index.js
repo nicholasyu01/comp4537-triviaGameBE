35e787e81614bb342367cfe09ed1f8e82526c559
nicholasyu01 <nyu@shaw.ca>
2021-11-04 02:22:08 -0700

    installed cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors')
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
@@ -8,6 +9,7 @@ const question = require("./controller/question");
 // const routes = require('./api/routes');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     next();
 });
 
